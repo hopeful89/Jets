94afcfc23ffe4f9b57ddf7929c275eb1583f5cf8
Hopeful89 <brandon.stine89@gmail.com>
2021-05-28 15:22:38 -0600

    first commit

---
+++ src/com/skilldistillery/jets/app/JetsApplication.java
@@ -0,0 +1,16 @@
+package com.skilldistillery.jets.app;
+
+import java.util.Scanner;
+
+import com.skilldistillery.jets.entity.AirField;
+
+public class JetsApplication {
+	private AirField airField;
+	private Scanner input = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/skilldistillery/jets/entity/AirField.java
@@ -0,0 +1,7 @@
+package com.skilldistillery.jets.entity;
+
+import java.util.List;
+
+public class AirField {
+	List<Jet> jet;
+}
